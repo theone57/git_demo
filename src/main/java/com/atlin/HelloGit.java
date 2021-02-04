25256fa47db2a3f0a52fb6819bf9d6f81111a06b
linpu <im.linpu@outlook.com>
2021-02-04 11:54:19 +0800

    first commit by git from atlin 2

---
+++ src/main/java/com/atlin/HelloGit.java
@@ -0,0 +1,15 @@
+package com.atlin;
+
+/**
+ * @author lin
+ * @version v1.0
+ * @program git_demo
+ * @description git test
+ * @date 2021-02-04 11:31
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git by atlin");
+    }
+}
+
